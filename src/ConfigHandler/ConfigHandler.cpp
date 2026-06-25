605250ce950486abe48d3d0830a09240d4710848
BlackHoleMX12892 <ikerpcamposr1283456@gmail.com>
2026-06-25 14:45:21 -0600

    Well, I fixed the issue with the config file not being found, but some optimizations on the config handler would be nice

--- src/ConfigHandler/ConfigHandler.cpp
+++ src/ConfigHandler/ConfigHandler.cpp
@@ -1,25 +1,26 @@
 #include "ConfigHandler.hpp"
 #include <toml++/toml.hpp>
 #include <vector>
+#include <filesystem>
 
 using namespace std::literals;
 
 std::vector<std::vector<std::string>> ConfigHandler::env;
+std::filesystem::path configFile = "ishellconfig.toml";
 
 
 void ConfigHandler::handleConfigFile() {
-    auto configfile = toml::parse_file("ishellconfig.toml");
+    if (std::filesystem::exists(configFile)) {
+        auto configfile = toml::parse_file("ishellconfig.toml");
 
-    auto* envtable = configfile["env"].as_table();
+        auto* envtable = configfile["env"].as_table();
 
-    if (envtable)
-    {
-        for (auto&& [key, value] : *envtable)
-        {
-            if (value.value<std::string>())
-            {
-                std::vector<std::string> temp = {std::string(key), std::string(value.value_or(""sv))};
-                env.push_back(temp);
+        if (envtable) {
+            for (auto&& [key, value] : *envtable) {
+                if (value.value<std::string>()) {
+                    std::vector<std::string> temp = {std::string(key), std::string(value.value_or(""sv))};
+                    env.push_back(temp);
+                }
             }
         }
     }
