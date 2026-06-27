56558bde7ea0cb008decc3f34875b30e53eab46f
BlackHoleMX12892 <ikerpcamposr1283456@gmail.com>
2026-06-27 10:00:04 -0600

    Added HOME replacing to the cwdhandler, making this a formatted path

--- src/CWDHandler/CWDHandler.cpp
+++ src/CWDHandler/CWDHandler.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <limits.h>
 #include <unistd.h>
+#include <sstream>
 
 std::string CWDHandler::getFormattedPath() { // will change to getFormattedPath() - done
     char buffer[PATH_MAX];
@@ -11,8 +12,35 @@ std::string CWDHandler::getFormattedPath() { // will change to getFormattedPath(
 
     std::string output = static_cast<std::string>(buffer);
 
-    if (output == getenv("HOME")) { // this will handle HOME replacing to ~ even when outside the HOME directory
+    /*if (output == getenv("HOME")) { // this will handle HOME replacing to ~ even when outside the HOME directory - done
         output = "~";
+    }*/
+
+    char *home = getenv("HOME");
+    std::string homestr = static_cast<std::string>(home);
+    std::stringstream outputstream;
+
+    int matches = 0;
+
+    for (size_t i = 0; i < homestr.size(); i++) {
+        if (output[i] == homestr[i])
+        {
+            matches++;
+        }
+    }
+
+    if (matches == homestr.size())
+    {
+        for (size_t i = 0; i < output.size() - homestr.size() + 1; i++)
+        {
+            if (i == 0)
+            {
+                outputstream << '~';
+                continue;
+            }
+            outputstream << output[(homestr.size() - 1) + (i)];
+        }
+        output = outputstream.str();
     }
 
     return output;
