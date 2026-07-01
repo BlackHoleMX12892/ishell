4e14673695659afea1cda656af71c2373bdf4db3
BlackHoleMX12892 <ikerpcamposr1283456@gmail.com>
2026-07-01 17:33:25 -0600

    Handle the command input and handle editing

--- src/LineEditor/LineEditor.cpp
+++ src/LineEditor/LineEditor.cpp
@@ -34,24 +34,42 @@ std::string LineEditor::readLine() {
         if (character == 1 && c == '\r')
         {
             std::cout << "\n\r" << std::flush;
-            //std::cout << command << std::flush;
             disableRaw();
             break;
         } else if (character == 1 && (c == '\b' || c == 127)) {
             if (!command.empty()) {
-                std::cout << "\b \b" << std::flush;
-                command.pop_back();
-                position--;
+                if (position == command.size())
+                {
+                    std::cout << "\b \b" << std::flush;
+                    command.pop_back();
+                    position--;
+                } else if (position > 0){
+                    std::stringstream backcharacters;
+                    command.erase(position - 1, 1);
+                    position--;
+                    for (int i = 0; i < command.size() - position; i++)
+                    {
+                        backcharacters << "\033[1D";
+                    }
+                    std::cout << "\b \b" << command.substr(position, command.size()) << std::flush;
+                    std::cout << " \b" << std::flush;
+                    std::cout << backcharacters.str() << std::flush;
+                }
             }
         } else if (character == 1 && c == '\033') {
             if (read(STDIN_FILENO, &c, 1) == 1 && c == '[') {
                 ssize_t character2 = read(STDIN_FILENO, &c, 1);
                 if (character2 == 1 && c == 'D') {
-                    std::cout << "\033[1D" << std::flush;
-                    position--;
+                    if (position > 0) {
+                        std::cout << "\033[1D" << std::flush;
+                        position--;
+                    }
                 } else if (character2 == 1 && c == 'C') {
-                    std::cout << "\033[1C" << std::flush;
-                    position++;
+                    if (position < command.size())
+                    {
+                        std::cout << "\033[1C" << std::flush;
+                        position++;
+                    }
                 } else if (character2 == 1 && c == 'A') {
                     //std::cout << "up" << std::flush;
                 } else if (character2 == 1 && c == 'B') {
@@ -59,7 +77,7 @@ std::string LineEditor::readLine() {
                 }
             }
         } else if (character == 1 && std::isprint(c)) {
-            if (position == (command.size())) { //position 0 only happens when no character
+            if (position == command.size()) {
                 std::cout << c << std::flush;
                 command.push_back(c);
                 position++;
